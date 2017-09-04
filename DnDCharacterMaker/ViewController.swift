ca43b1a36e2b8d0644dec9776366d7ff8c9f757e
octopused <octopused@gmail.com>
2017-09-04 11:43:06 +0500

    Initial Commit

---
+++ DnDCharacterMaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DnDCharacterMaker
+//
+//  Created by RuslanKa on 04.09.17.
+//  Copyright © 2017 RuslanKa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
